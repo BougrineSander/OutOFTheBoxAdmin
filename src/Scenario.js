b7c146bdacc5484f33ad8ceaec8e80905b5d5e32
Fré Dumazy <dumazy@gmail.com>
2019-11-22 19:25:55 +0100

    Add css

--- src/Scenario.js
+++ src/Scenario.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './App.css'
 
 export const Scenario = ({scenario, selected, onSelect}) => {
     const data = scenario.data();
+    const className = selected ? 'selected-scenario scenario' : 'scenario'
     return (
-        <div onClick={onSelect}>
-            {selected && <p>Currently:</p>}
+        <div onClick={onSelect} className={className}>
             <div>
                 <p>Order: {data.order}</p>
                 <p>{data.type}</p>
